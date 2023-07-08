38d4a5ab99004cf23046e1a3a1100d0c0023ef78
5kdk <5kdk.code@gmail.com>
2023-07-08 18:13:54 +0900

    feat: root 이벤트 바인딩 추가

--- dom/render.js
+++ dom/render.js
@@ -1,10 +1,20 @@
+import eventHolder from './eventHolder.js';
+
 let rootComponentInstance = null;
+
+const bindEventHandler = $root => {
+  for (const { type, selector, handler } of eventHolder) {
+    (selector === 'window' ? window : $root).addEventListener(type, handler);
+  }
+};
 
 const render = (RootComponent, $container) => {
   if (RootComponent) rootComponentInstance = new RootComponent();
 
   const domString = rootComponentInstance.render();
   $container.innerHTML = domString;
+
+  bindEventHandler($container);
 };
 
 export default render;
