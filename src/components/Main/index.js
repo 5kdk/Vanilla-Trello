22946f1e68e64f119c26af98daa28091157f1e0e
5kdk <5kdk.code@gmail.com>
2023-07-12 01:53:01 +0900

    feat: 기본 card UI 마크업

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,16 +8,22 @@ class Main extends Component {
     return `
       <main>
       ${trello.map(({ id, title, cards }, index) => `
-        <ul data-list-index="${index}" data-list-id="${id}">
-          <h1>${title}</h1>
-          ${cards.map(({id, title, description }) => `
-          <li data-card-id="${id}">
-            <h2>${title}</h2>
-            ${description && `<p>${description}</p>`}
-          </li>`).join('')}
-        </ul>`).join('')}
-        <div>
-          <button>+ Add a card</button>
+        <div class="lists-container">
+          <div class="list">
+            <div class="list-container">
+              <textarea class="list-title">${title}</textarea>
+              <ul data-list-index="${index}" data-list-id="${id}">
+              ${cards.map(({id, title }) => `
+                <li data-card-id="${id}" class="card">
+                  <h2 class="card-title">${title}</h2>
+                </li>`).join('')}
+              </ul>
+              <button class="card-composer-opener">+ Add a card</button>
+            </div>
+          </div>
+        </div>`).join('')}
+        <div class="list-composer-container">
+          <button class="list-composer-opener">+ Add a card</div>
         </div>
       </main>
       `;
