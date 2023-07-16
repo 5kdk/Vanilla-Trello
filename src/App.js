6078d5e3251c69a018d0ff521ac6086adbec1d95
5kdk <5kdk.code@gmail.com>
2023-07-16 22:54:51 +0900

    feat: 카드 생성기 on off 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Component from '../core/Component.js';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
-import { getTrelloState } from './state/trelloState.js';
+import { getTrelloState, setTrelloState } from './state/trelloState.js';
 
 class App extends Component {
   constructor() {
@@ -18,6 +18,39 @@ class App extends Component {
       ${header.render()}
       ${main.render()}
       `;
+  }
+
+  addEventListener() {
+    return [
+      {
+        type: 'beforeunload',
+        selector: 'window',
+        handler: () => setTrelloState(this.state),
+      },
+      {
+        type: 'click',
+        selector: null,
+        handler: this.onClick.bind(this),
+      },
+    ];
+  }
+
+  getListId($elem) {
+    return +$elem.closest('.list').dataset.listId;
+  }
+
+  toggleCardCreator(listId) {
+    const lists = this.state.lists.map(list =>
+      list.id === listId ? { ...list, isOpenCardCreator: !list.isOpenCardCreator } : list
+    );
+
+    this.setState({ lists });
+  }
+
+  onClick(e) {
+    if (e.target.matches('.card-creator-opener') || e.target.matches('.card-creator-closer')) {
+      this.toggleCardCreator(this.getListId(e.target));
+    }
   }
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -3,18 +3,27 @@ import Card from './Card.js';
 
 class List extends Component {
   render() {
-    const { data, i } = this.props;
-    const { title, id, cards } = data;
+    const { list, i } = this.props;
+    const { title, id, cards, isOpenCardCreator } = list;
 
     // prettier-ignore
     return `
-      <div class="list">
+      <div class="list" data-list-id="${id}">
         <div class="list-container">
           <textarea class="list-title">${title}</textarea>
           <ul data-list-index="${i}" data-list-id="${id}">
             ${cards.map(card => `${new Card(card).render()}`).join('')}
           </ul>
-          <button class="card-composer-opener">+ Add a card</button>
+          ${isOpenCardCreator ?
+          `<form class="card-creator">
+            <textarea class="new-card-title" placeholder="Enter a title for this card..." autofocus></textarea>
+            <div class="creator-controller">
+              <button class="btn">Add card</button>
+              <a class="bx bx-x bx-md card-creator-closer"></a>
+            </div>
+          </form>`
+          :
+          `<button class="card-creator-opener">+ Add a card</button>`}
         </div>
       </div>`;
   }
