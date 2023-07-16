64314f259bff8c41c637005bd55430edd36fbdc6
5kdk <5kdk.code@gmail.com>
2023-07-16 22:25:13 +0900

    feat: 수정, 모달 관련 상태 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,15 @@ import List from './List.js';
 
 class Main extends Component {
   render() {
-    const userData = this.props;
+    const { lists } = this.props;
 
     // prettier-ignore
     return `
       <main>
         <div class="lists-container">
-        ${userData.map((data, i) => `${new List({ data, i }).render()}`).join('')}
-          <div class="list-composer-container">
-            <button class="list-composer-opener">+ Add a card</div>
+        ${lists.map((list, i) => `${new List({ list, i }).render()}`).join('')}
+          <div class="list-creator-container">
+            <button class="list-creator-opener">+ Add a card</div>
           </div>
         </div>
       </main>`;
