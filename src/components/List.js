74741d4cabb2d57bf38002cb98a206a19ca3ac19
5kdk <5kdk.code@gmail.com>
2023-08-17 23:09:12 +0900

    fix: domstr 정렬

--- src/components/List.js
+++ src/components/List.js
@@ -14,16 +14,16 @@ class List extends Component {
           <ul data-list-index="${i}" data-list-id="${id}">
             ${cards.map(card => `${new Card(card).render()}`).join('')}
           </ul>
-          ${isOpenCardCreator ?
-          `<form class="card-creator">
+          ${isOpenCardCreator ? `
+          <form class="card-creator">
             <textarea class="new-card-title" placeholder="Enter a title for this card..." autofocus></textarea>
             <div class="creator-controller">
               <button class="btn">Add card</button>
               <a class="bx bx-x bx-md card-creator-closer"></a>
             </div>
           </form>`
-          :
-          `<button class="card-creator-opener">+ Add a card</button>`}
+          : `
+          <button class="card-creator-opener">+ Add a card</button>`}
         </div>
       </div>`;
   }
